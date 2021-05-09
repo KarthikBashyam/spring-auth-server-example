b01385f2737841f98d1eda5e37e2582f903837b3
karthikb4ps@gmail.com <karthikb4ps@gmail.com>
2021-05-09 17:11:36 -0400

    Initial Commit

---
+++ src/main/java/com/example/springauthserverexample/SpringAuthServerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springauthserverexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthServerExampleApplication.class, args);
+	}
+
+}
